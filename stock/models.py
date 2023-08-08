b7c96d1486c3091d2fe826a0c66d1b7d9130c1fa
Mehmet Akif Karaöz <karaozmehmetakif@gmail.com>
2023-08-08 10:11:58 +0300

    10. Fixed

--- stock/models.py
+++ stock/models.py
@@ -1,10 +1,7 @@
-from django.db import models
 
-
-# ---------------------------------
-# FixModel
-# ---------------------------------
-
+# ? ---------------------------------
+# ? FixModel
+# ? ---------------------------------
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -17,14 +14,13 @@ class FixModel(models.Model):
     class Meta:
         abstract = True
 
+# ? ---------------------------------
+# ? Models
+# ? ---------------------------------
 
-# ---------------------------------
-# Models
-# ---------------------------------
 
 class Category(FixModel):
-    name = models.CharField(max_length=64, unique=True)
-    image = models.URLField(blank=True, null=True)
+    name = models.CharField(max_length=64)
 
     def __str__(self):
         return self.name
@@ -40,7 +36,9 @@ class Brand(FixModel):
 
 class Product(FixModel):
     category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, related_name='brand_products')
+        Category, on_delete=models.CASCADE, related_name='category_products')
+    brand = models.ForeignKey(
+        Brand, on_delete=models.CASCADE, related_name='brand_products')
     name = models.CharField(max_length=128, unique=True)
     stock = models.SmallIntegerField(blank=True, default=0)
 
@@ -49,7 +47,7 @@ class Product(FixModel):
 
 
 class Firm(FixModel):
-    name = models.CharField(max_length=64, unique=True)
+    name = models.CharField(max_length=64)
     phone = models.CharField(max_length=16, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
     image = models.URLField(blank=True, null=True)
@@ -59,12 +57,29 @@ class Firm(FixModel):
 
 
 class Purchase(FixModel):
+    firm = models.ForeignKey(
+        Firm, on_delete=models.SET_NULL, null=True, related_name='firm_purchases')
+    brand = models.ForeignKey(
+        Brand, on_delete=models.CASCADE, related_name='brand_purchases')
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='product_purchases')
+    quantity = models.SmallIntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price_total = models.DecimalField(
+        max_digits=8, decimal_places=2, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.product} [+{self.quantity}]'
+
+
+class Sale(FixModel):
     brand = models.ForeignKey(
         Brand, on_delete=models.CASCADE, related_name='brand_sales')
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name='product_sales')
     quantity = models.SmallIntegerField()
-    price = models.DecimalField(
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price_total = models.DecimalField(
         max_digits=8, decimal_places=2, blank=True, null=True)
 
     def __str__(self):
